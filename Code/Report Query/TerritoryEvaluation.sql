6d022a618b2adb710d5da258dddb94764c5ae368
JaJa2006 <JaJa2006@users.noreply.github.com>
2025-01-31 04:44:23 +0000

    Automated updates by CI workflow

--- Code/Report Query/TerritoryEvaluation.sql
+++ Code/Report Query/TerritoryEvaluation.sql
@@ -5,9 +5,9 @@ SUM("SubTotal") AS "Sub Total",
 SUM("TaxAmt") AS "Tax Amt",
 SUM("Freight") AS "Freight",
 SUM("TotalDue") AS "Total Due"
-FROM SALES_SALESORDERHEADER_CLEANED soh
-LEFT JOIN SALES_SALESTERRITORY_CLEANED st
-ON st."TerritoryID" = soh."TerritoryID"
+FROM SALES_SALESORDERHEADER_CLEANED AS soh
+LEFT JOIN SALES_SALESTERRITORY_CLEANED AS st
+ON soh."TerritoryID" = st."TerritoryID"
 WHERE soh."SalesPersonID" IS NOT NULL
 GROUP BY st."Group"
 ORDER BY "Total Due" DESC, "Count Of Orders" DESC;
