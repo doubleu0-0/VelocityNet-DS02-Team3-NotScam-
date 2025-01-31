14750c158eb2308ccdabd318a06267460276e66e
Xue Cong <120545395+doubleu0-0@users.noreply.github.com>
2025-01-31 14:55:02 +0800

    Create ProductInventoryAnalysis.sql

---
+++ Code/Report Query/ProductInventoryAnalysis.sql
@@ -0,0 +1,17 @@
+-- Analyze stock levels and calculate total inventory value, and flag if the stock is low
+SELECT 
+    p."Name" AS Product,
+    i."LocationID",
+    i."Quantity" AS StockLevel,
+    (i."Quantity" * p."StandardCost") AS TotalInventoryValue,
+    p."SafetyStockLevel",
+    p."ReorderPoint",
+    CASE 
+        WHEN i."Quantity" < p."ReorderPoint" THEN 'Reorder Needed'
+        WHEN i."Quantity" >= p."SafetyStockLevel" THEN 'Stock is Safe'
+        ELSE 'Stock Level Warning'
+    END AS StockStatus
+FROM PRODUCTION_PRODUCTINVENTORY_CLEANED i
+JOIN PRODUCTION_PRODUCT_CLEANED p ON i."ProductID" = p."ProductID"
+WHERE i."Quantity"> 0
+ORDER BY TotalInventoryValue DESC;
