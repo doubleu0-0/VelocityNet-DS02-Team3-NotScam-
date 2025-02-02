ba050ac816c7802ed8d8c5e392baa035a49fb4c4
trashprog <trashprog@users.noreply.github.com>
2025-02-02 04:52:00 +0000

    Automated updates by CI workflow

--- Code/Report Query/PurchasingAnalysis.sql
+++ Code/Report Query/PurchasingAnalysis.sql
@@ -4,16 +4,16 @@ SELECT
     poh."VendorID", 
     v."AccountNumber" AS "VENDOR ACCOUNT", 
     '$' || SUM(poh."TotalDue") AS "TOTAL DUE" 
-FROM PURCHASING_PURCHASEORDERHEADER_CLEANED poh 
-JOIN PURCHASING_VENDOR_CLEANED v ON poh."VendorID" = v."BusinessEntityID"
+FROM PURCHASING_PURCHASEORDERHEADER_CLEANED AS poh 
+INNER JOIN PURCHASING_VENDOR_CLEANED AS v ON poh."VendorID" = v."BusinessEntityID"
 GROUP BY poh."VendorID", v."AccountNumber";
 
 --analyse the purchases made for each shipmethod
 SELECT 
-    sm."Name" AS "SHIPMETHOD", 
+    sm."Name" AS SHIPMETHOD, 
     sm."ShipMethodID", 
     COUNT(*) AS "PURCHASES MADE FOR SHIPMETHOD" 
-FROM PURCHASING_PURCHASEORDERHEADER_CLEANED poh 
-JOIN PURCHASING_SHIPMETHOD_CLEANED sm ON poh."ShipMethodID" = sm."ShipMethodID" 
+FROM PURCHASING_PURCHASEORDERHEADER_CLEANED AS poh 
+INNER JOIN PURCHASING_SHIPMETHOD_CLEANED AS sm ON poh."ShipMethodID" = sm."ShipMethodID" 
 GROUP BY sm."ShipMethodID", sm."Name"
 ORDER BY COUNT(*) DESC;
