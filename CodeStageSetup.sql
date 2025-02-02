a993c29dfdd535e972759e8cee05488e536d54da
Zachariah Loy <133225993+trashprog@users.noreply.github.com>
2025-02-02 20:02:08 +0800

    Update CodeStageSetup.sql

--- CodeStageSetup.sql
+++ CodeStageSetup.sql
@@ -15,11 +15,20 @@ CREATE OR REPLACE STORAGE INTEGRATION S3SNOWFLAKE_TEAM3
     STORAGE_AWS_ROLE_ARN = 'arn:aws:iam::851725251098:role/team3_snowflake_access_role'
     STORAGE_ALLOWED_LOCATIONS = ('s3://team3bucket/team3database/');
 
+--create another storage intergration for logging files
+CREATE OR REPLACE STORAGE INTEGRATION S3SNOWFLAKE_LOGGER_TEAM3
+  TYPE = EXTERNAL_STAGE
+  STORAGE_PROVIDER = 'S3'
+  ENABLED = TRUE
+  STORAGE_AWS_ROLE_ARN = 'arn:aws:iam::851725251098:role/team3_bucketlogsaccessrole'
+  STORAGE_ALLOWED_LOCATIONS = ('s3://team3bucket/team3databaselogs/')
+
 -- display storage integration
 SHOW STORAGE INTEGRATIONS;
 
 -- access the storage integration to get the arn and id
 DESC STORAGE INTEGRATION S3SNOWFLAKE_TEAM3;
+DESC STORAGE INTEGRATION S3SNOWFLAKE_LOGGER_TEAM3;
 
 -- create the file format for CSV
 CREATE OR REPLACE FILE FORMAT CSVFILEFORMATTEAM3
@@ -30,13 +39,19 @@ CREATE OR REPLACE FILE FORMAT CSVFILEFORMATTEAM3
     ERROR_ON_COLUMN_COUNT_MISMATCH = FALSE
     NULL_IF = ('NULL');
 
--- create the external stage object
+-- create the external stage object for the primary stage
 CREATE OR REPLACE STAGE S3_STAGE_TEAM3
     STORAGE_INTEGRATION = S3SNOWFLAKE_TEAM3
     URL = 's3://team3bucket/team3database/'
     FILE_FORMAT = CSVFILEFORMATTEAM3;
 
-LIST @S3_STAGE_TEAM3;
-DESCRIBE STAGE S3_STAGE_TEAM3;
+--create another stage referencing the logging storage intergration object, this will store the file upload logs
+CREATE OR REPLACE STAGE S3_STAGE_LOGGER_TEAM3
+  STORAGE_INTEGRATION = S3SNOWFLAKE_LOGGER_TEAM3
+  URL = 's3://team3bucket/team3databaselogs/'
+  FILE_FORMAT = CSVFILEFORMATTEAM3;
 
--- End file with a newline
+--list the stages to check if it works
+LIST @S3_STAGE_TEAM3;
+LIST @S3_STAGE_LOGGER_TEAM3;
+DESCRIBE STAGE S3_STAGE_TEAM3;
